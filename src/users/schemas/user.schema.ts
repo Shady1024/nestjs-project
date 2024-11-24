921053692321c1aa3cfa0b36c440894f4c74ff07
Shady1024 <shadykhaled2002@gmail.com>
2024-11-24 19:02:28 +0200

    commit

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,6 +1,18 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-   import { Document } from 'mongoose';
+   import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+   import mongoose, { Document } from 'mongoose';
+      
 
+
+   export type UserDocument = User & Document
+  
+
+   
+   export enum user_role {
+    student = 'student',
+    admin = 'admin',
+    instructor = 'instructor',
+  }
+  
    @Schema()
    export class User extends Document {
      @Prop({ required: true })
@@ -11,6 +23,16 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
      @Prop({ required: true })
      password: string;
+
+     @Prop({ required: true, unique: true })
+     user_id: mongoose.Types.ObjectId;
+
+     @Prop({ required: false })
+     mfa_secret?: string
+
+     @Prop({ required: true, enum: user_role })
+     role: string
+   
    }
 
    export const UserSchema = SchemaFactory.createForClass(User);
