bd6061a9823c1990cdda6a9fd99afe685e555377
Shady1024 <shadykhaled2002@gmail.com>
2024-11-24 19:32:08 +0200

    s

---
+++ src/users/schemas/progress.schemas.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+ 
+export type ProgressDocument = Progress & Document;
+
+ @Schema()
+export class Progress extends Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, default: function () { return this._id; }, unique: true })
+  progress_id: mongoose.Types.ObjectId;
+
+  @Prop({ required: true })
+  user_id: string;
+
+  @Prop({ required: true })
+  course_id: string;
+
+  @Prop({ required: true })
+  completion_percentage: number;
+
+  @Prop({ required: true, type: Date, default: Date.now })
+  last_accessed: Date;
+}
+
+export const ProgressSchema = SchemaFactory.createForClass(Progress);
